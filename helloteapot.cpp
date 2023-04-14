741e8ebe5ac1450b7150b0759e4179dc21a0ff06
Simone Gasparini <simone.gasparini@gmail.com>
2023-04-14 12:09:37 +0200

    format and clean

--- helloteapot.cpp
+++ helloteapot.cpp
@@ -2,86 +2,77 @@
 
 // for mac osx
 #ifdef __APPLE__
+#include <GLUT/glut.h>
 #include <OpenGL/gl.h>
 #include <OpenGL/glu.h>
-#include <GLUT/glut.h>
 #else
 // only for windows
 #ifdef _WIN32
 #include <windows.h>
 #endif
 // for windows and linux
+#include <GL/freeglut.h>
 #include <GL/gl.h>
 #include <GL/glu.h>
-#include <GL/freeglut.h>
 #endif
 
-
 // function called everytime the windows is refreshed
-void display () 
+void display()
 {
-
-    // clear window 
+    // clear window
     glClear(GL_COLOR_BUFFER_BIT);
 
-    // draw scene 
+    // draw scene
     glutWireTeapot(.5);
 
-    // flush drawing routines to the window 
+    // flush drawing routines to the window
     glFlush();
-
 }
 
 // Function called everytime a key is pressed
-void key( unsigned char key, int, int)
+void key(unsigned char key, int, int)
 {
-    switch ( key )
+    switch(key)
     {
-    	// the 'esc' key
+        // the 'esc' key
         case 27:
         // the 'q' key
-        case 'q':
-            exit( EXIT_SUCCESS );
-            break;
-        default:
-            break;
+        case 'q': exit(EXIT_SUCCESS); break;
+        default: break;
     }
-    glutPostRedisplay( );
+    glutPostRedisplay();
 }
 
 // Function called every time the main window is resized
-void reshape ( int width, int height )
+void reshape(int width, int height)
 {
     // define the viewport transformation;
-    glViewport(0,0,width,height);
+    glViewport(0, 0, width, height);
 }
 
-
 // Main routine
-int main ( int argc, char * argv[] ) 
+int main(int argc, char* argv[])
 {
+    // initialize GLUT, using any commandline parameters passed to the
+    //   program
+    glutInit(&argc, argv);
 
-    // initialize GLUT, using any commandline parameters passed to the 
-    //   program 
-    glutInit( &argc, argv );
+    // setup the size, position, and display mode for new windows
+    glutInitWindowSize(500, 500);
+    glutInitWindowPosition(0, 0);
+    glutInitDisplayMode(GLUT_RGB);
 
-    // setup the size, position, and display mode for new windows 
-    glutInitWindowSize( 500, 500 );
-    glutInitWindowPosition( 0, 0 );
-    glutInitDisplayMode( GLUT_RGB );
-
-    // create and set up a window 
+    // create and set up a window
     glutCreateWindow("Hello, teapot!");
-    
+
     // Set up the callback functions:
     // for display
-    glutDisplayFunc( display );
+    glutDisplayFunc(display);
     // for the keyboard
-    glutKeyboardFunc( key );
+    glutKeyboardFunc(key);
     // for reshaping
-    glutReshapeFunc( reshape );
+    glutReshapeFunc(reshape);
 
-
-    // tell GLUT to wait for events 
+    // tell GLUT to wait for events
     glutMainLoop();
 }
